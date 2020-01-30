fa1f1da851e2d9f163f92c4a99ee5d7e796e3818
SahiK411 <00389819@uca.edu.sv>
2020-01-30 10:25:22 -0600

    Removed: Search Fucntions

--- DiMoltos.cpp
+++ DiMoltos.cpp
@@ -9,9 +9,9 @@
 using namespace std;
 
 //ENUM declaration
-enum mainDish{pizza = 0, pasta, lasagna, n};
-enum drink{beer = 0, tea, soda, o};
-enum appetizer{pizzaRolls = 0, garlicBread, cheeseSticks, ne};
+enum mainDish{pizza = 0, pasta, lasagna, none};
+enum drink{beer = 0, tea, soda, nada};
+enum appetizer{pizzaRolls = 0, garlicBread, cheeseSticks, no};
 enum paymentMethod{cash = 0, creditCard, null};
 
 //Structs
@@ -57,6 +57,8 @@ vector <takeOut> takeOrder;
 vector <inHouseOrder> houseOrder;
 vector <takeOut> dispatchedTakeOut;
 vector <inHouseOrder> dispatchedHouse;
+int globalIDTake = 1;
+int globalIDHouse = 1;
 string printDish[] = {"Pizza", "Pasta", "Lasagna", "none"};
 string printAppe[] = {"Rollos de Pizza", "Pan con ajo", "Palitos de queso", "none"};
 string printDrink[] = {"Cerveza", "Te Helado", "Gaseosa", "none"};
@@ -197,6 +199,7 @@ void mainMenu(bool *outerCheck){
 
 void addTakeOut(){
     takeOut auxOrder;
+    auxOrder.id = globalIDTake++;
     int aux;
     bool cont = true;
     //Adding orders 'to go'/delivery.
@@ -281,6 +284,7 @@ void addTakeOut(){
         
 void addHouseOrder(){
     inHouseOrder auxOrder;
+    auxOrder.id = globalIDHouse++;
     auxOrder.price = 0;
     bool cont = true;
     int aux;
@@ -352,6 +356,7 @@ void addHouseOrder(){
     }while(aux != 1 && aux != 2);
 
     cout << "Su total es: " << auxOrder.price << endl;
+    houseOrder.insert(houseOrder.end(), auxOrder);
 }
 
 void showTakeOuts(){
@@ -368,15 +373,15 @@ void showTakeOuts(){
         cout << "Numero del cliente: " << takeOrder[i].clientPhoneNumber << endl;
         cout << "Plato(s) principal(es): " << endl;
         for(int j = 0; j < takeOrder[i].orderDish.size(); j++){
-            cout << j << ": " << printDish[takeOrder[i].orderDish[j]] << endl;
+            cout << j + 1 << ": " << printDish[takeOrder[i].orderDish[j]] << endl;
         }
         cout << "Aperitivo(s): " << endl;
         for(int j = 0; j < takeOrder[i].orderAppe.size(); j++){
-            cout << j << ": " << printDish[takeOrder[i].orderAppe[j]] << endl;
+            cout << j + 1 << ": " << printDish[takeOrder[i].orderAppe[j]] << endl;
         }
         cout << "Bebida(s): " << endl;
         for(int j = 0; j < takeOrder[i].orderDrink.size(); j++){
-            cout << j << ": " << printDish[takeOrder[i].orderDrink[j]] << endl;
+            cout << j + 1 << ": " << printDish[takeOrder[i].orderDrink[j]] << endl;
         }
         cout << "Precio: " << takeOrder[i].price << endl;
         cout << "Metodo de pago: " << printMethod[takeOrder[i].orderMethod] << endl;
@@ -394,15 +399,15 @@ void showHouseOrders(){
         cout << "Personas en la mesa: " << houseOrder[i].clientsPerTable << endl;
         cout << "Plato(s) principal(es): " << endl;
         for(int j = 0; j < houseOrder[i].orderDish.size(); j++){
-            cout << j << ": " << printDish[houseOrder[i].orderDish[j]] << endl;
+            cout << j + 1 << ": " << printDish[houseOrder[i].orderDish[j]] << endl;
         }
         cout << "Aperitivo(s): " << endl;
         for(int j = 0; j < houseOrder[i].orderAppe.size(); j++){
-            cout << j << ": " << printDish[houseOrder[i].orderAppe[j]] << endl;
+            cout << j + 1 << ": " << printDish[houseOrder[i].orderAppe[j]] << endl;
         }
         cout << "Bebida(s): " << endl;
         for(int j = 0; j < houseOrder[i].orderDrink.size(); j++){
-            cout << j << ": " << printDish[houseOrder[i].orderDrink[j]] << endl;
+            cout << j + 1 << ": " << printDish[houseOrder[i].orderDrink[j]] << endl;
         }
         cout << "Precio: " << houseOrder[i].price << endl;
         cout << "Metodo de pago: " << printMethod[houseOrder[i].orderMethod] << endl;
@@ -419,265 +424,6 @@ void showTotalPrice(){
         totalPrice += dispatchedHouse[i].price;
     }
     cout << "Las ganancias de hoy son: " << totalPrice << "$\n";
-}
-
-void searchByNameTake(){
-    //Function to find order by first name, last name
-    string searchedName, searchedLast;
-    bool found = false, cont = true;
-    int temp = 0, userInput;
-    appetizer auxAppe;
-    mainDish auxDish;
-    drink auxDrink;
-    cout << "Ingrese el primer nombre de la orden que desea encontrar.\n";
-    getline(cin, searchedName);
-    cout << "Ingrese el ultimo apellido de la orden que desea encontrar.\n";
-    getline(cin, searchedLast);
-
-    for(int i = 0; i < takeOrder.size(); i++){
-        int aux = 0;
-        if(searchedName == takeOrder[i].clientName.firstName && searchedLast == takeOrder[i].clientName.lastName){
-            found = true;
-            cout << "Orden encontrada! El identificador de la orden es: " << takeOrder[i].id << " .\n";
-            cout << "Desea cambiar algo?\n 1- Si\t 2- No\n";
-            cin >> userInput;
-            if(userInput == 1){
-                while(temp != 6){
-                    cout << "Digite el numero de la opcion deseada.\n";
-                    cout << "\t1- Nombre." << endl;
-                    cout << "\t2- Direccion." << endl;
-                    cout << "\t3- Numero de telefono." << endl;
-                    cout << "\t4- Orden." << endl;
-                    cout << "\t5- Metodo de pago." << endl;
-                    cout << "\t6- Regresar." << endl;
-                    cin >> temp;
-                    switch(temp){
-                        case 1:
-                            cout << "Por favor digite su nombre.\n Ingrese el primer nombre.\n";
-                            cin.ignore();
-                            getline(cin, takeOrder[i].clientName.firstName);
-
-                            cout << "Ingrese el segundo nombre.\n";
-                            getline(cin, takeOrder[i].clientName.middleName);
-
-                            cout << "Ingrese el primer apellido.\n";
-                            getline(cin, takeOrder[i].clientName.surName);
-
-                            cout << "Ingrese el segundo apellido.\n";
-                            getline(cin, takeOrder[i].clientName.lastName);
-                            break;
-                        case 2:
-                            cout << "Por favor digite su direccion.\n Ingrese el numero de casa.\n";
-                            cin.ignore();
-                            getline(cin, takeOrder[i].clientAddress.houseNumber);
-
-                            cout << "Ingrese la colonia.\n";
-                            getline(cin, takeOrder[i].clientAddress.neighborhood);
-
-                            cout << "Ingrese el municipio.\n";
-                            getline(cin, takeOrder[i].clientAddress.county);
-
-                            cout << "Ingrese el departamento.\n";
-                            getline(cin, takeOrder[i].clientAddress.state);
-                            break;
-                        case 3:
-                            cout << "Por favor digite su numero de telefono, con el formato XXXX-XXXX: \n";
-                            cin.ignore();
-                            getline(cin, takeOrder[i].clientPhoneNumber);
-                            break;
-                        case 4:
-                            takeOrder[i].price = 0;
-                            takeOrder[i].orderDish.clear();
-                            takeOrder[i].orderDish.shrink_to_fit();
-                            takeOrder[i].orderAppe.clear();
-                            takeOrder[i].orderAppe.shrink_to_fit();
-                            takeOrder[i].orderDrink.clear();
-                            takeOrder[i].orderDrink.shrink_to_fit();
-                            do{
-                                cont = true;
-                                menuAppe(&(takeOrder[i]));
-                                cout << "Continuar?\n \t1. Si\n \t2. No\n";
-                                cin >> aux;
-                                if(aux == 2){
-                                    cont = false;
-                                }
-                            }while(cont);
-                            do{
-                                cont = true;
-                                menuDish(&(takeOrder[i]));
-                                cout << "Continuar?\n \t1. Si\n \t2. No\n";
-                                cin >> aux;
-                                if(aux == 2){
-                                    cont = false;
-                                }
-                            }while(cont);
-
-                            do{
-                                cont = true;
-                                menuDrink(&takeOrder[i]);
-                                cout << "Continuar?\n \t1. Si\n \t2. No\n";
-                                cin >> aux;
-                                if(aux == 2){
-                                    cont = false;
-                                }
-                            }while(cont);
-                            break;
-                        case 5:
-                            do{
-                                cout << "Metodo de pago\n";
-                                cout << "\t1. Efectivo\n";
-                                cout << "\t2. Tarjeta\n";
-                                cout << "Su opcion: ";
-                                cin >> aux;
-                                cin.ignore();
-                                switch(aux){
-                                    case 1:
-                                        takeOrder[i].orderMethod = paymentMethod::cash;
-                                        break;
-                                    case 2:
-                                        takeOrder[i].orderMethod = paymentMethod::creditCard;
-                                        break;
-                                    default:
-                                        break;
-                                }
-                            }while(aux != 1 && aux != 2);
-                            break;
-                        case 6:
-                            cin.ignore();
-                            break;
-                        default:
-                            cout << "error" << endl;
-                            break;
-                    }
-                }
-            }
-            else{}
-        }
-    }
-    if(found == false){
-        cout << "No se encontro esa orden.\n";
-    }
-}
-
-void searchByNameHouse(){
-    //Function to find order by first name, last name
-    string searchedName, searchedLast;
-    bool found = false, cont = true;
-    int temp = 0, userInput;
-    int aux = 0;
-    appetizer auxAppe;
-    mainDish auxDish;
-    drink auxDrink;
-    cout << "Ingrese el primer nombre de la orden que desea encontrar.\n";
-    getline(cin, searchedName);
-    cout << "Ingrese el ultimo apellido de la orden que desea encontrar.\n";
-    getline(cin, searchedLast);
-
-    for(int i = 0; i < houseOrder.size(); i++){
-        if(searchedName == houseOrder[i].clientName.firstName && searchedLast == houseOrder[i].clientName.lastName){
-            cout << "Orden encontrada! El identificador de la orden es: " << houseOrder[i].id << " .\n";
-            cout << "Desea cambiar algo?\n 1- Si\t 2- No\n";
-            cin >> userInput;
-            if(userInput == 1){
-                while(temp != 6){
-                    cout << "Digite el numero de la opcion deseada.\n";
-                    cout << "\t1- Nombre." << endl;
-                    cout << "\t2- Numero de clientes en la mesa." << endl;
-                    cout << "\t3- Orden." << endl;
-                    cout << "\t4- Metodo de pago." << endl;
-                    cout << "\t5- Regresar." << endl;
-                    cin >> temp;
-                    switch(temp){
-                        case 1:
-                            cout << "Por favor digite su nombre.\n Ingrese el primer nombre.\n";
-                            cin.ignore();
-                            getline(cin, houseOrder[i].clientName.firstName);
-
-                            cout << "Ingrese el segundo nombre.\n";
-                            getline(cin, houseOrder[i].clientName.middleName);
-
-                            cout << "Ingrese el primer apellido.\n";
-                            getline(cin, houseOrder[i].clientName.surName);
-
-                            cout << "Ingrese el segundo apellido.\n";
-                            getline(cin, houseOrder[i].clientName.lastName);
-                            break;
-                        case 2:
-                            cout << "Por favor digite el numero de clientes en la mesa: \n";
-                            cin.ignore();
-                            cin >> houseOrder[i].clientsPerTable;
-                            break;
-                        case 3:
-                            houseOrder[i].price = 0;
-                            houseOrder[i].orderDish.clear();
-                            houseOrder[i].orderDish.shrink_to_fit();
-                            houseOrder[i].orderAppe.clear();
-                            houseOrder[i].orderAppe.shrink_to_fit();
-                            houseOrder[i].orderDrink.clear();
-                            houseOrder[i].orderDrink.shrink_to_fit();
-                            do{
-                                cont = true;
-                                menuAppe(&(houseOrder[i]));
-                                cout << "Continuar?\n \t1. Si\n \t2. No\n";
-                                cin >> aux;
-                                if(aux == 2){
-                                    cont = false;
-                                }
-                            }while(cont);
-                            do{
-                                cont = true;
-                                menuDish(&(houseOrder[i]));
-                                cout << "Continuar?\n \t1. Si\n \t2. No\n";
-                                cin >> aux;
-                                if(aux == 2){
-                                    cont = false;
-                                }
-                            }while(cont);
-                            do{
-                                cont = true;
-                                menuDrink(&(houseOrder[i]));
-                                cout << "Continuar?\n \t1. Si\n \t2. No\n";
-                                cin >> aux;
-                                if(aux == 2){
-                                    cont = false;
-                                }
-                            }while(cont);
-                            break;
-                        case 4:
-                            do{
-                                cout << "Metodo de pago\n";
-                                cout << "\t1. Efectivo\n";
-                                cout << "\t2. Tarjeta\n";
-                                cout << "Su opcion: ";
-                                cin >> aux;
-                                cin.ignore();
-                                switch(aux){
-                                    case 1:
-                                        houseOrder[i].orderMethod = cash;
-                                        break;
-                                    case 2:
-                                        houseOrder[i].orderMethod = creditCard;
-                                        break;
-                                    default:
-                                        break;
-                                }
-                            }while(aux != 1 && aux != 2);
-                            break;
-                        case 5:
-                            cin.ignore();
-                            break;
-                        default:
-                            cout << "error" << endl;
-                            break;
-                    }
-                }
-            }
-            else{}
-        }
-    }
-    if(found == false){
-        cout << "No se encontro la orden.\n";
-    }
 }
 
 void deleteOrderTakeOut(){
